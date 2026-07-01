bc2e8ef4856c58408192e296ed3464857dd920c1
Eric <gricgrick@gmail.com>
2026-07-01 19:42:16 -0300

    adicionando campos de endereço ao model Cliente

--- pedidos/models.py
+++ pedidos/models.py
@@ -4,6 +4,12 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     telefone = models.CharField(max_length=20)
+    rua = models.CharField(max_length=255)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    cep = models.CharField(max_length=9)
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
